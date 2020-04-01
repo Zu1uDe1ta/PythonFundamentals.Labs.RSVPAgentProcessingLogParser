e8bfe2e627789c4f4f330722fc40db7d581db1aa
Zu1uDe1ta <chrischavezdataengineer@gmail.com>
2020-03-31 22:59:43 -0400

    changes had been made that were not initially added

--- log_parser.py
+++ log_parser.py
@@ -1,13 +1,13 @@
+
+
 import re
 
 file_name = '/Users/cchavez/dev/PythonFundamentals.Labs.RSVPAgentProcessingLogParser/data/rsvp_agent_log.dat'
-
 print("WARNINGS:")
-
 with open(file_name, "r+") as data_file:
-   for line in data_file:
-      match = re.search("WARNING", line)
-      if match is None:
+   for lines in data_file:
+      match = re.search("WARNING", lines)
+      if match == None:
          pass
       else:
-         print(str(line[0:14]) + " -- " + str(line[45:]))
+         print(str(lines[0:14]) + " -- " + str(re.split(lines[45:]))
